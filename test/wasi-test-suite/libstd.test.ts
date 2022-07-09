62c12ac0e0a47f52eb9c37bdfe213909e6584932
Yuta Saito <kateinoigakukun@gmail.com>
2022-07-09 15:05:54 +0000

    add libstd test suite

---
+++ test/wasi-test-suite/libstd.test.ts
@@ -0,0 +1,32 @@
+import { readdirSync, statSync } from "fs";
+import { join as pathJoin } from "path";
+import { runTest } from "./harness";
+
+describe("wasi-test-suite-libstd", () => {
+    const suiteDir = pathJoin(__dirname, "../../third_party/wasi-test-suite/libstd");
+    const entries = readdirSync(suiteDir);
+    const UNSUPPORTED = [
+        "fs_create_dir-new-directory.wasm",
+        "fs_file_create.wasm",
+        "fs_metadata-directory.wasm",
+        "fs_metadata-file.wasm",
+        "fs_rename-directory.wasm",
+        "fs_rename-file.wasm",
+        "env_args-many.wasm",
+        "env_args-none.wasm",
+        "env_args-some.wasm",
+        "env_vars-many.wasm",
+    ]
+
+    for (const entry of entries) {
+        const filePath = pathJoin(suiteDir, entry)
+        const stat = statSync(filePath);
+        if (!entry.endsWith(".wasm") || !stat.isFile()) {
+            continue;
+        }
+        const defineCase = UNSUPPORTED.includes(entry) ? it.skip : it;
+        defineCase(entry, async () => {
+            await runTest(filePath);
+        })
+    }
+})
