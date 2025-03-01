0ab8f459553031abf44bab2f005309b964336df5
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-01 02:39:16 +0000

    Use useMemoryFS in useAll by default

--- src/features/all.ts
+++ src/features/all.ts
@@ -3,27 +3,22 @@ import { WASIFeatureProvider, WASIOptions } from "../options";
 import { useArgs } from "./args";
 import { useClock } from "./clock";
 import { useEnviron } from "./environ";
-import { useFS, useStdio } from "./fd";
+import { useMemoryFS } from "./fd";
 import { useProc } from "./proc";
 import { useRandom } from "./random";
 
-type Options = (Parameters<typeof useFS>[0] | Parameters<typeof useStdio>[0]) &
-  Parameters<typeof useRandom>[0];
+type Options = Parameters<typeof useMemoryFS>[0] & Parameters<typeof useRandom>[0];
 
 export function useAll(useOptions: Options = {}): WASIFeatureProvider {
   return (options: WASIOptions, abi: WASIAbi, memoryView: () => DataView) => {
     const features = [
+      useMemoryFS(useOptions),
       useEnviron,
       useArgs,
       useClock,
       useProc,
       useRandom(useOptions),
     ];
-    if ("fs" in useOptions) {
-      features.push(useFS({ fs: useOptions.fs }));
-    } else {
-      features.push(useStdio(useOptions));
-    }
     return features.reduce((acc, fn) => {
       return { ...acc, ...fn(options, abi, memoryView) };
     }, {});
