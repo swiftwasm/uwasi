ef2387898a1f16899acd758461c02dc236c64125
Andrew Sampson <1297077+andrewmd5@users.noreply.github.com>
2025-04-10 00:41:50 +0900

    fix: bundler issues
    typescript / bundlers expect things to be defined inside the exports definition
    
    fix: node deps
    
    fix: make app responsible for providing the implementation
    
    chore: bump node

--- src/features/random.ts
+++ src/features/random.ts
@@ -1,6 +1,5 @@
 import { WASIAbi } from "../abi.js";
 import { WASIFeatureProvider } from "../options.js";
-import { defaultRandomFillSync } from "../platforms/crypto.js";
 
 /**
  * Create a feature provider that provides `random_get` with `crypto` APIs as backend by default.
@@ -10,7 +9,7 @@ export function useRandom(
     randomFillSync?: (buffer: Uint8Array) => void;
   } = {},
 ): WASIFeatureProvider {
-  const randomFillSync = useOptions.randomFillSync || defaultRandomFillSync;
+  const randomFillSync = useOptions.randomFillSync || crypto.getRandomValues;
   return (options, abi, memoryView) => {
     return {
       random_get: (bufferOffset: number, length: number) => {
--- src/platforms/crypto.ts
+++
@@ -1,5 +0,0 @@
-import * as crypto from "crypto";
-
-export const defaultRandomFillSync = (buffer: Uint8Array) => {
-  crypto.randomFillSync(buffer);
-};
